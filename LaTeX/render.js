aa0abf4bf01a287862187967f23929a44c639101
Endothermic-Dragon-ED <eshaandebnath@gmail.com>
2022-06-22 16:04:38 -0400

    Added variance graph

--- LaTeX/render.js
+++ LaTeX/render.js
@@ -3,6 +3,7 @@ const xmlserializer = require('xmlserializer');
 
 const source = process.argv[2]
 const output = process.argv[3]
+const scale = 15
 
 async function latexToSVG(){
   let { render } = require("./renderer.js");
@@ -16,10 +17,10 @@ async function latexToSVG(){
     var parser = require('parse5'),
         dom = parser.parse(htmlString).childNodes[0].childNodes[1].childNodes[0].childNodes[0];
     let oldWidth = dom.attrs.find(el => el.name == "width").value
-    dom.attrs.find(el => el.name == "width").value = parseFloat(oldWidth.slice(0,-2))*100 + "px"
+    dom.attrs.find(el => el.name == "width").value = parseFloat(oldWidth.slice(0,-2))*scale + "px"
     // console.log(dom.attrs.find(el => el.name == "width"))
     let oldHeight = dom.attrs.find(el => el.name == "height").value
-    dom.attrs.find(el => el.name == "height").value = parseFloat(oldHeight.slice(0,-2))*100 + "px"
+    dom.attrs.find(el => el.name == "height").value = parseFloat(oldHeight.slice(0,-2))*scale + "px"
     // console.log(dom.attrs.find(el => el.name == "height"))
 
     return xmlserializer.serializeToString(dom);
--- Plots/Linear Regression/main.py
+++ Plots/Linear Regression/main.py
@@ -6,13 +6,13 @@ os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 def renderBiasGraph():
     data = np.array([
+        [0.796, 2.2],
         [1.304, 3.4],
         [3.07, 8.6],
         [5.65, 9.9],
         [6.76, 8.2],
         [6.93, 8.6],
         [7.38, 6.9],
-        [0.796, 2.2],
         [8.84, 4.1]
     ])
 
@@ -35,4 +35,33 @@ def renderBiasGraph():
     plt.savefig("output/bias.png", bbox_inches='tight')
     plt.show()
 
-renderBiasGraph()
+def renderVarianceGraph():
+    data = np.array([
+        [1.04, 3.42],
+        [3.71, 3.68],
+        [5.15, 2.65],
+        [7.42, 3.16],
+        [9.63, 5.14],
+        [12.17, 4.69],
+        [14.24, 6.55],
+        [16.3, 7.61],
+        [18.27, 7.02]
+    ])
+
+    quadraticFit = [0.0144182, -0.00572898, 3.11042]
+    eighthFit = [0.00000400708, -0.000305728, 0.00961241, -0.160818, 1.54259, -8.51759, 25.7954, -37.964, 22.9627]
+
+    inputs = np.arange(0, 20, 0.1)
+
+    quadraticOutputs = np.sum([quadraticFit[i]*inputs**(2-i) for i in range(len(quadraticFit))], 0)
+    eighthOutputs = np.sum([eighthFit[i]*inputs**(8-i) for i in range(len(eighthFit))], 0)
+
+    plt.plot(*data.T, "kx")
+    plt.plot(inputs, quadraticOutputs, "b")
+    plt.plot(inputs, eighthOutputs, "r")
+    plt.axis([0, 20, 0, 15])
+    plt.axis('off')
+    plt.savefig("output/variance.png", bbox_inches='tight')
+    plt.show()
+
+renderVarianceGraph()
